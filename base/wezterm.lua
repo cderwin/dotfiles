2f408e729feff2757888e8cd66f9fe9588bf43d0
Cameron Derwin <cderwin@palantir.com>
2025-10-17 16:12:01 -0700

    Restore old wezterm bindings

--- base/wezterm.lua
+++ base/wezterm.lua
@@ -8,19 +8,39 @@ config.font_size = 12
 config.leader = { key = '`' }
 config.keys = {
     {
-	key = '`',
-	mods = 'LEADER',
-	action = wezterm.action.SendKey { key = '`' },
+        key = '`',
+        mods = 'LEADER',
+        action = wezterm.action.SendKey { key = '`' },
     },
     {
-	key = '%',
-	mods = 'LEADER',
-	action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+        key = '%',
+        mods = 'LEADER',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
     },
     {
-	key = '"',
-	mods = 'LEADER',
-	action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+        key = '"',
+        mods = 'LEADER',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = 'm',
+        mods = 'LEADER',
+        action = wezterm.action.TogglePaneZoomState,
+    },
+    {
+        key = '[',
+        mods = 'LEADER',
+        action = wezterm.action.ActivateTabRelative(-1),
+    },
+    {
+        key = ']',
+        mods = 'LEADER',
+        action = wezterm.action.ActivateTabRelative(1),
+    },
+    {
+        key = 'p',
+        mods = 'LEADER',
+        action = wezterm.action.PaneSelect,
     },
 }
 
