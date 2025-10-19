0e4c88090cc86be95ea0ed5a13a8126584cd2e56
Cameron Derwin <cderwin@palantir.com>
2025-10-19 00:41:00 -0700

    add wezterm nushell config and pane hotkeys

--- base/wezterm.lua
+++ base/wezterm.lua
@@ -5,6 +5,12 @@ local config = wezterm.config_builder()
 config.color_scheme = "ForestBlue"
 config.font_size = 12
 
+config.default_prog = { "/Users/cderwin/.local/bin/nu" }
+config.set_environment_variables = {
+    XDG_CONFIG_HOME = "/Users/cderwin/.config",
+    XDG_DATA_DIR = "/Users/cderwin/.lcal/share",
+}
+
 config.leader = { key = '`' }
 config.keys = {
     {
@@ -41,6 +47,16 @@ config.keys = {
         key = 'p',
         mods = 'LEADER',
         action = wezterm.action.PaneSelect,
+    },
+    {
+        key = 'r',
+        mods = 'LEADER',
+        action = wezterm.action.PaneSelect { mode = 'SwapWithActiveKeepFocus' },
+    },
+    {
+        key = 'p',
+        mods = 'LEADER',
+        action = wezterm.action.PaneSelect { mode = 'MoveToNewTab' },
     },
 }
 
